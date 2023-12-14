45067569cc9f69fd52255ae7787519eb717228a4
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2023-12-14 13:48:31 +0700

    Add files via upload

---
+++ asdasdasd.cpp
@@ -0,0 +1,25 @@
+/* LANG : C++ */
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+int main() {
+    int row, i, j;
+    string sign;
+
+    cin >> row;
+    cin >> sign;
+    for (i = row; i >= 1; i--) {
+        if (i >= 1) {
+            for (j = 1; j <= i; j++) {
+                if (j <= i) {
+                    cout << sign << endl;
+                }
+            }
+        }
+        cout << "\n" << endl;
+    }
+    return 0;
+}
